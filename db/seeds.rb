e93f76366ee638b5bd12f33b196e848eb540c9af
Nicolas LEONARD <n7eonard@gmail.com>
2017-11-22 14:29:31 +0100

    Seed with 5 good restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "L'Estaminet", address: "116 Rue Oberkampf, 75011 Paris", phone_number: "01 43 57 34 29", category: "french")
+Restaurant.create(name: "Le Jumeyrah", address: "101 Rue Saint-Maur, 75011 Paris", phone_number: "01 43 14 08 69", category: "belgian")
+Restaurant.create(name: "Restaurant Darna", address: "127 Boulevard de Ménilmontant, 75011 Paris", phone_number: "09 54 61 15 19", category: "chinese")
+Restaurant.create(name: "Les Niçois", address: "7 Rue Lacharrière, 75011 Paris", phone_number: "09 84 16 55 03", category: "italian")
+Restaurant.create(name: "O' woks", address: "58 Boulevard Voltaire, 75011 Paris", phone_number: "01 48 06 55 55", category: "japanese")
+
+
+
